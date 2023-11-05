e4a0d14fbc4daf973e2d24f762f75670895df35f
mattget21 <matthew.getachew@bison.howard.edu>
2023-11-04 20:22:33 -0500

    formatted data for frontend

--- duffel.js
+++ duffel.js
@@ -28,37 +28,52 @@ function searchFlights(origin, destination, departureDate, passengers) {
     data: requestData
   };
 
-axios.request(config)
+  axios.request(config)
   .then((response) => {
     if (response.data && response.data.data && Array.isArray(response.data.data.offers)) {
       const allOffers = response.data.data.offers;
-      const filteredOffers = allOffers.filter(offer => {
+      const flightOptions = allOffers.map(offer => {
         // Assuming each offer has at least one slice and each slice has at least one segment
-        return !offer.slices.some(slice => 
-          slice.segments.some(segment => 
-            segment.operating_carrier.name === 'American Airlines'
-          )
-        );
+        const firstSlice = offer.slices[0];
+        const firstSegment = firstSlice.segments[0];
+        const lastSegment = firstSlice.segments[firstSlice.segments.length - 1];
+
+        // Map the API response to the FlightOptionProps structure
+        const flightOption = {
+          departureTime: firstSegment.departure_time,
+          arrivalTime: lastSegment.arrival_time,
+          flightNumber: parseInt(firstSegment.marketing_carrier_flight_number, 10),
+          departureAirport: firstSegment.departure_airport,
+          arrivalAirport: lastSegment.arrival_airport,
+          gate: firstSegment.departure_gate || 'N/A', // Assuming gate information is optional
+          stops: firstSlice.segments.length - 1, // Assuming direct flight has 1 segment
+          prices: {
+            firstClass: 0, // You will need to map these based on the API response
+            business: 0, // You will need to map these based on the API response
+            mainCabin: parseFloat(offer.total_amount) // Assuming total_amount is for main cabin
+          }
+        };
+
+        return flightOption;
       });
 
-      // Get only the first 5 offers after filtering
-      const firstFiveOffers = filteredOffers.slice(0, 5);
-
-      firstFiveOffers.forEach((offer, index) => {
-        // Log the offer details here
-        console.log(`Offer ${index + 1}:`, offer);
-      });
+      // Return the mapped flight options
+      console.log(flightOptions);
+      return flightOptions;
     } else {
       console.error('Unexpected data structure for offers:', JSON.stringify(response.data, null, 2));
+      return []; // Return an empty array if the structure is not as expected
     }
   })
   .catch((error) => {
     console.error('Error:', error.response ? error.response.data : error.message);
+    return []; // Return an empty array in case of error
   });
 
 }
 
 // Test:
+//THESE WILL BE INPUT DYNAMICALLY 
 searchFlights(
   'NYC', // Origin
   'ATL', // Destination
